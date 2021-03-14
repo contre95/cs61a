f4013ce0ce5a478b18b126ec9b587f6f2ee606f4
contre95ml <lucas.contreras@mercadolibre.com>
2021-03-14 20:34:27 -0300

    Can't solve Q3 gp

---
+++ optionals/cs61a.org-examprep-examprep03/wait_its_all_palindromes.py
@@ -0,0 +1,20 @@
+# Given a string s, return the longest palindromic substring of s. If there are multiple palindromes of greatest length, then return the leftmost one. You may not use is_palindrome.
+# Hint: Given equivalent values a and b, max(a, b) will evaluate to a. You may also find the key argument to max helpful.
+def greatest_pal_two(s):
+    """
+    >>> greatest_pal_two("tenet")
+    'tenet'
+    >>> greatest_pal_two("tenets")
+    'tenet'
+    >>> greatest_pal_two("stennet")
+    'tennet'
+    >>> greatest_pal_two("abc")
+    'a'
+    >>> greatest_pal_two("")
+    ''
+    """
+    for i in _______:
+        if i:
+            return  ________________________________________________________________________
+    return s
+
